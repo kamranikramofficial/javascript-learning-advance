7d8241c56e2cf443a07c38712f80b5cb6ae429fb
kamranikramofficial <kamranikram706@gmail.com>
2025-04-24 12:12:11 +0500

    add window our scope

--- class-1/app.js
+++ class-1/app.js
@@ -1,6 +1,7 @@
 // code Execution
 // callstack
 // different between undifine or is not defined
+// window
 // variable scoping
 
 
@@ -29,5 +30,65 @@
 // undifine
 // undifine jab ata ha jab memory phase excute hona per value defult undefine save hogate ha our code phase ma us ko updated value nahi milte
 // is not defined
-// is not defined jab ata ha memory phase execute hogata ha our us ko variable nahi milta our code phase ma hum us variable ka istaml karta ha to ya error ata ha  
-// 
+// is not defined jab ata ha memory phase execute hogata ha our us ko variable nahi milta our code phase ma hum us variable ka istaml karta ha to ya error ata ha
+
+// window
+// The window object is the global object of JavaScript when run within a browser and represents the browser window. All browsers support it. It contains as members all global JavaScript objects, functions, and variables. Such variables become properties of the window object, such functions become methods of the window object.
+// Below shows the use of a window method, .alert(), to display a message to the user:
+// window.alert('Hello World!');
+// Because the window object is global, the “window.” portion can also be omitted:
+// alert('Hello World!');
+
+
+// variable scoping
+// Scope determines the accessibility (visibility) of variables.
+// JavaScript variables have 3 types of scope:
+// Block scope
+// Function scope
+// Global scope
+
+// Block scope
+// Before ES6 (2015), JavaScript variables had only Global Scope and Function Scope.
+// ES6 introduced two important new JavaScript keywords: let and const.
+// These two keywords provide Block Scope in JavaScript.
+// Variables declared inside a { } block cannot be accessed from outside the block:
+
+// {
+//     let x = 2;
+// }
+// x can NOT be used here
+
+// Variables declared with the var keyword can NOT have block scope.
+// Variables declared inside a { } block can be accessed from outside the block.
+// {
+//     var x = 2;
+//   }
+//   // x CAN be used here
+
+
+// Function scope
+// Variables declared within a JavaScript function, are LOCAL to the function:
+
+// code here can NOT use carName
+// function myFunction() {
+//     let carName = "Volvo";
+// code here CAN use carName
+//   }
+// code here can NOT use carName
+  
+// Local variables have Function Scope:
+// They can only be accessed from within the function.
+// JavaScript has function scope: Each function creates a new scope.
+// Variables defined inside a function are not accessible (visible) from outside the function.
+// Variables declared with var, let and const are quite similar when declared inside a function.
+
+
+// Global scope
+// A variable declared outside a function, becomes GLOBAL.
+// A global variable has Global Scope:
+// // All scripts and functions on a web page can access it. 
+// Variables declared Globally (outside any function) have Global Scope.
+// Global variables can be accessed from anywhere in a JavaScript program.
+// Variables declared with var, let and const are quite similar when declared outside a block.
+// They all have Global Scope
+
