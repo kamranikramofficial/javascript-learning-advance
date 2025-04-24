2effa15a7502e3738d8d56b7548ae90e06798c58
kamranikramofficial <kamranikram706@gmail.com>
2025-04-24 11:51:23 +0500

    add undifine or not define

--- class-1/app.js
+++ class-1/app.js
@@ -1,22 +1,20 @@
-// code Execution 
+// code Execution
 // callstack
-//different between undifine or not define
-// variable scoping 
+// different between undifine or is not defined
+// variable scoping
 
 
-// what is javascript 
-// javascript is a single thread and synchronous language
+// what is javascript
+// javascript is a single thread and synchronous language which meane he run the code line by line of code.
 
 // javascript code Execution
-// The execution context in JavaScript is an abstract concept that defines the environment in which code is executed. It is a crucial part of understanding how JavaScript works behind the scenes. Whenever JavaScript code runs, it does so within an execution context. 
+// The execution context in JavaScript is an abstract concept that defines the environment in which code is executed. It is a crucial part of understanding how JavaScript works behind the scenes. Whenever JavaScript code runs, it does so within an execution context.
 // javascript code execute in two phace memory and code and run first memory phase
-
-//Memory 
+//Memory
 // The memory phase of the JavaScript execution context is the initial stage where the JavaScript engine sets up the environment for code execution. It is also called the "variable environment".
 // memory stage ma variable init phase ma save hota ha our us ke value undifine save hote ha our pher code phase jab chalta ha to us line of code ka chlna ka bade value update ho kar jo apna  value rhakhe ha yo ho gate ha
-// memory stage ma funtion key and value ka pere ma pura fuction mathord ke tarha store hotha hota ha like fuction name : pura fuction ka under ka line of code 
-
-//code 
+// memory stage ma funtion key and value ka pere ma pura fuction mathord ke tarha store hotha hota ha like fuction name : pura fuction ka under ka line of code
+//code
 // The code execution phase in which the variables are assigned their value and the code is run line by line .
 // The code execution phase update the value to undifine sa actual value .
 // The code execution phase function create your own code execution context in which same process . use callstack.
@@ -27,3 +25,9 @@
 // When a function is called, it's added to the top of the call stack. When that function finishes executing, it's removed from the stack, and control returns to the function below it. This process continues until the stack is empty, indicating that all function calls have completed.
 // The call stack plays a vital role in managing the flow of execution in JavaScript programs. It ensures that functions are executed in the correct order and that control is returned to the appropriate location after each function call.
 
+// undifine or not define
+// undifine
+// undifine jab ata ha jab memory phase excute hona per value defult undefine save hogate ha our code phase ma us ko updated value nahi milte
+// is not defined
+// is not defined jab ata ha memory phase execute hogata ha our us ko variable nahi milta our code phase ma hum us variable ka istaml karta ha to ya error ata ha  
+// 
