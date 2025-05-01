a130aa030449fdb086413f0694bc52e0d7f7dc3c
kamranikramofficial <kamranikram706@gmail.com>
2025-05-01 20:27:57 +0500

    class -3

---
+++ class-3/app.js
@@ -0,0 +1,18 @@
+// closure 
+// A closure in JavaScript is a function that remembers its outer variables and can access them, even after the outer function has finished executing. This is possible because the inner function retains a reference to the outer function's scope, which is known as the lexical environment.
+
+// Example
+
+// function outer() {
+//     let outerVar = "I'm in the outer scope!";
+//     function inner() {
+//         console.log(outerVar);      lexical environment
+//     }
+//     return inner;
+// }
+// const closure = outer(); 
+// closure();
+
+// A function along with it's lexical environment its called a closure.
+
+
